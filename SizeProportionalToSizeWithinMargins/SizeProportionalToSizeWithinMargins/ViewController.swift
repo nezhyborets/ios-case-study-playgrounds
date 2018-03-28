0159fc65ce7c61d0f47cac93a80047fb6dfb4f89
Nezhyborets Oleksii <nezhyborets@macpaw.com>
2018-03-28 12:09:05 +0300

    SizeProportionalToSizeWithinMargins - add SnapKit cases to the test

--- SizeProportionalToSizeWithinMargins/SizeProportionalToSizeWithinMargins/ViewController.swift
+++ SizeProportionalToSizeWithinMargins/SizeProportionalToSizeWithinMargins/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
 
@@ -17,6 +18,7 @@ class ViewController: UIViewController {
         v1.backgroundColor = .yellow
         v1.text = "superview"
         NSLayoutConstraint.activate([
+            v1.topAnchor.constraint(equalTo: view.topAnchor),
             v1.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             v1.heightAnchor.constraint(equalTo: view.heightAnchor)
             ])
@@ -25,6 +27,7 @@ class ViewController: UIViewController {
         v2.backgroundColor = .red
         v2.text = "safeAreaLayoutGuide"
         NSLayoutConstraint.activate([
+            v2.topAnchor.constraint(equalTo: view.topAnchor),
             v2.leadingAnchor.constraint(equalTo: v1.trailingAnchor),
             v2.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.heightAnchor),
             v2.widthAnchor.constraint(equalTo: v1.widthAnchor)
@@ -34,10 +37,46 @@ class ViewController: UIViewController {
         v3.backgroundColor = .blue
         v3.text = "layoutMarginsGuide"
         NSLayoutConstraint.activate([
+            v3.topAnchor.constraint(equalTo: view.topAnchor),
             v3.leadingAnchor.constraint(equalTo: v2.trailingAnchor),
             v3.heightAnchor.constraint(equalTo: view.layoutMarginsGuide.heightAnchor),
             v3.widthAnchor.constraint(equalTo: v2.widthAnchor)
             ])
+
+        let v4 = baseLabel()
+        v4.backgroundColor = .yellow
+        v4.text = "snp.superview"
+        v4.snp.makeConstraints { maker in
+            maker.top.equalToSuperview()
+            maker.leading.equalTo(v3.snp.trailing)
+            maker.height.equalToSuperview()
+            maker.width.equalTo(v3)
+        }
+
+        let v5 = baseLabel()
+        v5.backgroundColor = .red
+        v5.text = "snp.safeAreaLayoutGuide"
+        v5.snp.makeConstraints { (maker) in
+            maker.top.equalToSuperview()
+            maker.leading.equalTo(v4.snp.trailing)
+            maker.height.equalTo(self.view.safeAreaLayoutGuide)
+            maker.width.equalTo(v4)
+        }
+
+        let v6 = baseLabel()
+        v6.backgroundColor = .blue
+        v6.text = "snp.layoutMarginsGuide"
+        v6.snp.makeConstraints { (maker) in
+            maker.top.equalToSuperview()
+            maker.leading.equalTo(v5.snp.trailing)
+            maker.height.equalTo(self.view.layoutMarginsGuide)
+            maker.width.equalTo(v5)
+        }
+
+        let lastView = v6
+        lastView.snp.makeConstraints { (maker) in
+            maker.trailing.equalToSuperview()
+        }
     }
 
     private func baseLabel() -> UILabel {
@@ -46,7 +85,6 @@ class ViewController: UIViewController {
         v1.adjustsFontSizeToFitWidth = true
         v1.minimumScaleFactor = 0.5
         view.addSubview(v1)
-        v1.topAnchor.constraint(equalTo: view.topAnchor)
         return v1
     }
 }
