84d40f8b777da3b0ca9fe3133003d0ffcdbbc976
Nezhyborets Oleksii <nezhyborets@macpaw.com>
2019-11-28 14:40:32 +0200

    Add ShadowCornerPerformance project

---
+++ ShadowCornerPerformance/ShadowCornerPerformance/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ShadowCornerPerformance
+//
+//  Created by Nezhyborets Oleksii on 7/16/18.
+//  Copyright © 2018 nezhyborets. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var scrollView: UIScrollView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
