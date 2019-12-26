f5e5697009b937af88f85b5c3f31794a2850ed3d
Nezhyborets Oleksii <nezhyborets@macpaw.com>
2019-12-26 13:24:25 +0200

    CoreDataSaveWithoutChanges

---
+++ CoreDataSaveWithoutChangesConflict/CoreDataSaveWithoutChangesConflict/SomeEntity+CoreData.swift
@@ -0,0 +1,15 @@
+//
+//  TestEntity+CoreData.swift
+//  CoreDataSaveWithoutChangesConflict
+//
+//  Created by Nezhyborets Oleksii on 12/26/19.
+//  Copyright © 2019 nezhyborets. All rights reserved.
+//
+
+extension SomeEntity {
+    public override func willSave() {
+        super.willSave()
+
+        print("willSave")
+    }
+}
